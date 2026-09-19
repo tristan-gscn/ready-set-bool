55d6831b2dfa78157d0761461aa572949eaf4213
tristan-gscn <tristan.gscn@proton.me>
2026-09-19 21:33:57 +0200

    chore: set up base project structure

---
+++ src/bin/ex00.rs
@@ -0,0 +1,12 @@
+fn adder(a: u32, b: u32) -> u32 {
+    42 // placeholder, not implemented yet
+}
+
+fn main() {
+    println!("{}", adder(3, 5));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
